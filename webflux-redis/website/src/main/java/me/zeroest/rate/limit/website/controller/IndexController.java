8b9d789598a81870fb870f72af439b6baca38c1a
zeroest <zeroest.me@gmail.com>
2025-08-31 16:29:06 +0900

    [webflux-redis] set index.html

---
+++ webflux-redis/website/src/main/java/me/zeroest/rate/limit/website/controller/IndexController.java
@@ -0,0 +1,14 @@
+package me.zeroest.rate.limit.website.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+
+}
