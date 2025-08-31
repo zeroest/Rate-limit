553fd8ca0c8cb995101d054604f65944d6177658
zeroest <zeroest.me@gmail.com>
2025-08-31 16:22:23 +0900

    [webflux-redis] init website

---
+++ webflux-redis/website/src/test/java/me/zeroest/rate/limit/website/WebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package me.zeroest.rate.limit.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
