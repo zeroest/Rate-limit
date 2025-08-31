1f35cdd7204a80c67090dd233f53e366eebf6e18
zeroest <zeroest.me@gmail.com>
2025-08-31 16:54:45 +0900

    [webflux-redis] init flow

---
+++ webflux-redis/flow/src/test/java/me/zeroest/rate/limit/flow/FlowApplicationTests.java
@@ -0,0 +1,13 @@
+package me.zeroest.rate.limit.flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
